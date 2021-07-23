f3b63acba1b2f342e19678e96509c92fdf2dfd8a
garzoand <garzoand@gmail.com>
2021-07-23 16:35:23 -0700

    added env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# You should run this as follows: ". ./env.sh"
+export GOOGLE_APPLICATION_CREDENTIALS="~/credentials.json"
+export GOOGLE_PROJECT="tf-gcp-student-XXX"
+
+# On windows
+# $env:GOOGLE_APPLICATION_CREDENTIALS="c:\Users\xxx\Download\xxx.json"
+# $env:GOOGLE_PROJECT="tf-gcp-student-XXX"
+
